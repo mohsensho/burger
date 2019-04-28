bc184d01f1c4201329a2f0ef0642e0d6c580889c
Mohsensho <shojaeifar.mo@yahoo.com>
2019-04-28 00:52:03 -0700

    front-end and back-end are completed

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('American Colossal burger', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Cheese burger', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Chicken Burger', false);
